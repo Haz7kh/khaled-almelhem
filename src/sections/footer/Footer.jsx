ade711cfe087eb5931ecd32fce2c8851d93debe9
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2023-07-29 10:03:48 +0200

    footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import { links, socials } from "./data";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <h2>Footer</h2>
+      <div className="container footer-container">
+        <ul className="nav-menu">
+          {links.map((fLink) => (
+            <li key={fLink.id}>
+              <a href={fLink.link}>{fLink.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="footer-socials">
+          {socials.map((social) => (
+            <a
+              key={social.id}
+              href={social.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {social.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <small>2023 Khaled Almelhem Portfolio &copy; All Rights Reserved</small>
+      </div>
     </footer>
   );
 };
