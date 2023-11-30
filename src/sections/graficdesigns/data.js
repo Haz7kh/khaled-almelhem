0d7c7b5d895bafc56ee13f4031e9fe1278d8a71c
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2023-11-30 13:39:39 +0300

    added some gf

--- src/sections/graficdesigns/data.js
+++ src/sections/graficdesigns/data.js
@@ -1,6 +1,9 @@
 import design1 from "../../assets/post2.png";
 import design2 from "../../assets/gd/postAd.png";
 import design3 from "../../assets/gd/profile.png";
+import design4 from "../../assets/gd/birthday.png";
+import design5 from "../../assets/gd/newcolllection.png";
+import design6 from "../../assets/gd/postermodel.png";
 
 const designs = [
   {
@@ -17,6 +20,21 @@ const designs = [
     id: 3,
     title: "Post",
     image: design3,
+  },
+  {
+    id: 4,
+    title: "Poster",
+    image: design4,
+  },
+  {
+    id: 5,
+    title: "Poster",
+    image: design5,
+  },
+  {
+    id: 6,
+    title: "Poster",
+    image: design6,
   },
 ];
 
