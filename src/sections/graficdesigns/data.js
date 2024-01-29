d0e15b6b052174e2c22f60939a245474bb47b6b6
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2024-01-29 21:15:59 +0300

    added some designs

--- src/sections/graficdesigns/data.js
+++ src/sections/graficdesigns/data.js
@@ -11,6 +11,11 @@ import design10 from "../../assets/gd/3d-sence.png";
 import design11 from "../../assets/gd/abajour.png";
 import design12 from "../../assets/gd/perfum.png";
 import design13 from "../../assets/gd/tube.png";
+import design14 from "../../assets/gd/jambottle.png";
+import design15 from "../../assets/gd/robot.png";
+import design16 from "../../assets/gd/owlbox.png";
+import design17 from "../../assets/gd/shapes-logo.png";
+import design18 from "../../assets/gd/shap-logo02.png";
 
 const designs = [
   {
@@ -77,6 +82,31 @@ const designs = [
     id: 13,
     title: "3D Product",
     image: design13,
+  },
+  {
+    id: 14,
+    title: "3D Bottle",
+    image: design14,
+  },
+  {
+    id: 15,
+    title: "3D Robot",
+    image: design15,
+  },
+  {
+    id: 16,
+    title: "3D Box",
+    image: design16,
+  },
+  {
+    id: 17,
+    title: "2D/3D - Logos ",
+    image: design17,
+  },
+  {
+    id: 18,
+    title: "Illustrator Shape ",
+    image: design18,
   },
 ];
 
