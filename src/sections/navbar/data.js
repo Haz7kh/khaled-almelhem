8c2228d14d57990224f146214ab051f20fa18881
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2024-02-18 17:03:12 +0300

    update

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -3,7 +3,6 @@ const data = [
   { id: 2, link: "#about", title: "About" },
   { id: 3, link: "#services", title: "Services" },
   { id: 4, link: "#portfolio", title: "Portfolio" },
-  { id: 6, link: "#graphic", title: "Graphics" },
   { id: 5, link: "#contact", title: "Contact" },
 ];
 
